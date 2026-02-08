2a320013815c8db5c99e6d02a2d0aaea5a6a0e96
Fatii78 <fatii78jj@gmail.com>
2026-02-08 08:56:09 +0300

    select a random word from an array

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const words = {
+const Words = {
   ANIMALS: ["COW", "HORSE", "DOLPHIN", "OWL", "FOX", "ZEBRA", "ELEPHANT", "MONKEY", "KANGAROO"],
   FOODS: ["TOMATO", "RICE", "SALAD", "SOUP", "PANCAKE", "PIZZA", "PASTA", "YOGURT", "CARROT"],
   JOBS: ["ENGINEER", "POLICE", "CHEF", "FARMER", "TEACHER", "DRIVER", "WAITER", "ACTOR", "DOCTOR"]
@@ -13,3 +13,9 @@ categoryTitle.textContent = selectedCategory
 backB.addEventListener("click", () => {
   window.location.href = "home.html"
 })
+
+const categoryWords = Words[selectedCategory]
+const random = Math.floor(Math.random() * categoryWords.length)
+const secretWord = categoryWords[random]
+console.log(selectedCategory, secretWord)
+
